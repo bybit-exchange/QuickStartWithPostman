4ce8e044dbe3349ada196ec7b53bcc87ccf49564
root <root@ip-172-18-1-37.ap-southeast-1.compute.internal>
2021-08-10 06:55:21 +0000

    new file:   bybitApiMonitoring.js

--- newmanVersion/bybitApiMonitoring.js
+++ newmanVersion/bybitApiMonitoring.js
@@ -1,9 +1,9 @@
 const newman = require('newman');
 const slackHandler=require('./slackHandler.js');
-//setInterval(
-//newman => newman.run({
-newman.run({
-	collection: require('./Bybit API Monitoring.postman_collection.json'),
+const request=require('request');
+setInterval(
+newman => newman.run({
+	collection: require('./Bybit\ API\ Monitoring.postman_collection.json'),
 	environment: require('./Bybit.postman_environment.json'),
 	reporters: "json"
 }).on('start', function (err, args) { // on start of run, log to console
@@ -28,7 +28,8 @@ newman.run({
 			else if(JSON.parse(ele.response.stream.toString()).hasOwnProperty('ret_code') & JSON.parse(ele.response.stream.toString()).ret_code>0){
 				slackHandlerInst.postMsg(ele.request.method+" "+ele.request.url.protocol+"://"+ele.request.url.host.join(".")+"/"+ele.request.url.path.join("/")+"?"+queryString+"\n"+ele.response.stream.toString())
 			}
-			console.log(ele.response.responseTime);
+			console.log('Push '+ele.request.method+'_'+ele.request.url.path.join("_")+' '+ele.response.responseTime.toString());
+			sendMetricsPushGateWay("APIMonitoring",ele.request.method+'_'+ele.request.url.path.join("_"),ele.response.responseTime);
 		}
 	);
 	if (err || summary.error) {
@@ -37,8 +38,7 @@ newman.run({
 	else {
 		console.log('collection run completed.');
 	}
-}
-//}),5000,newman
+}),5000,newman
 );
 
 function GetSortOrder(prop) {    
@@ -51,3 +51,12 @@ function GetSortOrder(prop) {
         return 0;    
     }    
 } 
+
+function sendMetricsPushGateWay(jobName,metricName,metricValue){
+	request({
+		url: 'http://localhost:9091/metrics/job/'+jobName,
+		method: 'POST',
+		body: metricName+' '+ metricValue.toString()+'\n',
+		encoding: null
+	}, (error, response, body) => {});
+}
