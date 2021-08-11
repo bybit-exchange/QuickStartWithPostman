681213358cbeca5a6b5d906f2ae99629ca56baad
root <root@ip-172-18-1-37.ap-southeast-1.compute.internal>
2021-08-11 02:06:22 +0000

    modified:   runNewManAsLib.js

--- newmanVersion/runNewManAsLib.js
+++ newmanVersion/runNewManAsLib.js
@@ -1,9 +1,6 @@
 const newman = require('newman');
-const slackHandler=require('./slackHandler.js');
-//setInterval(
-//newman => newman.run({
 newman.run({
-	collection: require('./Bybit API Monitoring.postman_collection.json'),
+	collection: require('./Bybit USDT_Perp for Newman.postman_collection.json'),
 	environment: require('./Bybit.postman_environment.json'),
 	reporters: "json"
 }).on('start', function (err, args) { // on start of run, log to console
@@ -11,7 +8,6 @@ newman.run({
 }).on('done', function (err, summary) {
 	summary.run.executions.forEach(ele=>
 		{
-			slackHandlerInst=new slackHandler("C026YBF16PQ","xxxx");
 			var queryString="";
 			ele.request.url.query.members.sort(GetSortOrder('key')).forEach(
 				ele2=>{
@@ -19,16 +15,7 @@ newman.run({
 				}
 			)
 			queryString=queryString.substring(0, queryString.length - 1);
-			if(ele.hasOwnProperty('requestError')){
-				slackHandlerInst.postMsg(ele.request.method+" "+ele.request.url.protocol+"://"+ele.request.url.host.join(".")+"/"+ele.request.url.path.join("/")+"?"+queryString+"\n"+"Response code "+ele.requestError);
-			}
-			else if(ele.response.code !=200){
-				slackHandlerInst.postMsg(ele.request.method+" "+ele.request.url.protocol+"://"+ele.request.url.host.join(".")+"/"+ele.request.url.path.join("/")+"?"+queryString+"\n"+"Response code "+ele.response.code.toString())
-			}
-			else if(JSON.parse(ele.response.stream.toString()).hasOwnProperty('ret_code') & JSON.parse(ele.response.stream.toString()).ret_code>0){
-				slackHandlerInst.postMsg(ele.request.method+" "+ele.request.url.protocol+"://"+ele.request.url.host.join(".")+"/"+ele.request.url.path.join("/")+"?"+queryString+"\n"+ele.response.stream.toString())
-			}
-			console.log(ele.response.responseTime);
+			console.log(queryString);
 		}
 	);
 	if (err || summary.error) {
@@ -38,7 +25,6 @@ newman.run({
 		console.log('collection run completed.');
 	}
 }
-//}),5000,newman
 );
 
 function GetSortOrder(prop) {    
